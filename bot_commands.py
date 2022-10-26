2e42eeb46d7a2789a854c0af9b9cb369302bbf11
Maksim <md.sinikov@gmail.com>
2022-10-26 17:45:30 +0500

    Доделал дз

--- bot_commands.py
+++ bot_commands.py
@@ -2,32 +2,40 @@ from datetime import datetime
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from datetime import datetime
+from spy import log
 
 async def zdarova_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log(update,context)
     await update.message.reply_text(f'Zdarova {update.effective_user.first_name}!')
 
 async def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log(update,context)
     await update.message.reply_text(f'{datetime.now().time()}')
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log(update,context)
     await update.message.reply_text('/hello\n/help\n/time\n/sum\n/sub\n/div\n/mult')
 
 async def sum_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log(update,context)
     msg = update.message.text
     items = msg.split()
     await update.message.reply_text(f'{items[1]} + {items[2]} = {int(items[1])+int(items[2])}')
     
 async def sub_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log(update,context)
     msg = update.message.text
     items = msg.split()
     await update.message.reply_text(f'{items[1]} - {items[2]} = {int(items[1])-int(items[2])}')
 
 async def div_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log(update,context)
     msg = update.message.text
     items = msg.split()
     await update.message.reply_text(f'{items[1]} / {items[2]} = {int(items[1])/int(items[2])}')
 
 async def mult_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log(update,context)
     msg = update.message.text
     items = msg.split()
     await update.message.reply_text(f'{items[1]} * {items[2]} = {int(items[1])*int(items[2])}')
